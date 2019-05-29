84d9d146b6e630221e9febbac3c71009a76e2802
BespalovSergey <50576059+BespalovSergey@users.noreply.github.com>
2019-05-29 14:34:02 +0300

    Update log_handler.py

--- log_handler.py
+++ log_handler.py
@@ -7,12 +7,11 @@ import os
 class MyLogsHandler(logging.Handler):
     def __init__(self,my_chat_id):
         super().__init__()
-
         self.error_bot = telegram.Bot(token= os.environ['bot_error_token'])
         self.my_chat_id = my_chat_id
+
     def emit(self, record):
         log_entry = self.format(record)
-
         bot_down = 'Бот telegtam_dialog упал с ошибкой \n'
         error_message = '{}{}'.format(bot_down, log_entry)
 
