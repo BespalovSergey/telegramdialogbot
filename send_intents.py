9acf0d55e8491a44f38f832f6cf635051df87250
BespalovSergey <50576059+BespalovSergey@users.noreply.github.com>
2019-05-29 14:35:11 +0300

    Update send_intents.py

--- send_intents.py
+++ send_intents.py
@@ -1,66 +1,72 @@
 import os
-import dialogflow_v2  as dialogflow
 import requests
+import logging
+from log_handler import MyLogsHandler
 
-
-
-
-
-def post_intents(logger):
-
-  url = os.enviro['train_phrases']
-  try:
-    response = requests.get(url = url)
-    response.raise_for_status()
-    phrases = response.json()
+def raise_response( response , logger):
+  
+  if response.raise_for_status():
     
-    googl_url = 'https://api.dialogflow.com/v1/intents?v=20150910'
+    logger.info('Произошла ошибка при передачи интента')
 
-    headers = {
+
+def post_intents():
+
+  logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message) s ')
+  logger = logging.getLogger('bot_logger')
+  logger.setLevel(logging.INFO)
+  logger.addHandler(MyLogsHandler(my_chat_id = os.environ['telegram_chat_id']))
+
+  url = os.environ['train_phrases']
+
+  response = requests.get(url = url)
+  raise_response(response,logger)
+  phrases = response.json()
+  
+  
+  googl_url = 'https://api.dialogflow.com/v1/intents/'
+  headers = {
     "Authorization":os.environ['dialog_dev_token_v1'],
     'Content-Type': 'application/json'
     }
     
-    for phrase in phrases:
-      user_says = []
-      for user_phras in phrases[phrase]['questions']:
-        says = {
-          'count':0,
-          'data':[
-            {
-              'text':user_phras
-           
-            }
-          ]
- 
-        }
-        user_says.append(says)
-      
-      data={
-        'auto':True,
-        'context':[],
-        'name':phrase,
+  for phrase in phrases:
+    user_says = []
 
-
-        'responses':[
+    for user_phras in phrases[phrase]['questions']:
+      says = {
+        'count':0,
+        'data':[
           {
-            
-            'messages':[{
-              
-              'speech':phrases[phrase]['answer'],
-              'type':0
-            }
-            
-            ]
+            'text':user_phras
           }
-        ],
-        'userSays': user_says
-
+        ]
       }
-      google_response = requests.post(url = googl_url,  headers = headers,json= data)
+      user_says.append(says)
       
-  except (ConnectionError ,requests.exceptions.HTTPError) as err:
-    logger.info(err)
+    data={
+      'v':'20150910',
+      'auto':True,
+      'context':[],
+      'name':phrase,
+      'responses':[
+        {
+          'messages':[{
+            'speech':phrases[phrase]['answer'],
+            'type':0
+          }
+          ]
+        }
+      ],
+      'userSays': user_says
+      }
+        
+    google_response = requests.post(url = googl_url,  headers = headers,json= data  )
+    raise_response(google_response,logger)
+ 
+
+if __name__ == '__main__':
+  post_intents()  
   
   
   
