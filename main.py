8696adc26588badd7e4f0199ee6b8921f8192ff4
BespalovSergey <50576059+BespalovSergey@users.noreply.github.com>
2019-05-29 14:32:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,10 @@ from telegram_dialog import telegram_bot
 import logging
 import os
 import requests
-import send_intents
 from vk_dialog import vk_bot
-import random
-
-
 
 def main():
-  telegram_bot()
-  vk_bot()
-  
-  
-if __name__ == '__main__':
-  
+
   logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message) s ')
   logger = logging.getLogger('bot_logger')
   logger.setLevel(logging.INFO)
@@ -24,5 +15,15 @@ if __name__ == '__main__':
   logger.info('Бот проверки ошибок запущен')
 
   load_dotenv()
+
+  try:
+    telegram_bot()
+    vk_bot()
+  except (ConnectionError  ,requests.exceptions.HTTPError) as err:
+    logger.info(err)
+    
+  
+  
+if __name__ == '__main__':
   main()
-  #send_intents.post_intents(logger)
+  
