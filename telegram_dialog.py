999b0f97d2624190f7a5715fc0ca5bbf684f10d2
BespalovSergey <50576059+BespalovSergey@users.noreply.github.com>
2019-05-29 14:35:34 +0300

    Update telegram_dialog.py

--- telegram_dialog.py
+++ telegram_dialog.py
@@ -2,12 +2,15 @@ from telegram.ext import Updater ,CommandHandler ,MessageHandler ,Filters
 import os
 from answer_dialogflow import get_dialog_answer
 import logging
+
+
 def start( bot , update):
     bot.send_message(chat_id = update.message.chat_id , text = 'Здравствуйте!')
 
 def echo(bot, update):
-  answer_message = get_dialog_answer(update.message.chat_id,update.message.text,True)
+  answer_message = get_dialog_answer(update.message.chat_id,update.message.text,'Извините, я вас не понял')
   update.message.reply_text(answer_message)
+
 def telegram_bot():
   updater = Updater(token= os.environ['telegram_token'])
   dispatcher = updater.dispatcher
